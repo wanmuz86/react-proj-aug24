df678e473282e2f7281bdafd651560a39fb8b11f
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-30 14:44:10 +0800

    Add Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Cart.css'
+import { useCart } from '../../libs/context/CartContext'
 
 const Cart = () => {
+  const { state } = useCart();
   return (
-    <div>Cart</div>
+    <div>
+      <h2>Cart</h2>
+      {
+        state.cart.length === 0 ?
+          <p>No item in cart at the moment.</p>
+          :
+          <ul>{
+            state.cart.map(val => <li key={val.id}>{val.item.title} - {val.quantity}</li>)
+          }
+          </ul>
+      }
+    </div>
   )
 }
 
